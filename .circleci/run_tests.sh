cfcfabd48297862b7e4e75adbe057b75735243b6
Habib LAFET <habib.lafet-ext@ledger.fr>
2020-11-24 18:38:27 +0100

    fix ci issue

--- .circleci/run_tests.sh
+++ .circleci/run_tests.sh
@@ -2,4 +2,10 @@ cd ../lib-ledger-core-build
 export MOCK_HTTP_FOLDER=`pwd`/../lib-ledger-core/core/test/fixtures/mocked_http/
 echo "mocked tests:"
 ls $MOCK_HTTP_FOLDER  
-~/cmake-3.16.5-Linux-x86_64/bin/ctest -VV -timeout 120
+if [ "$1" == "linux" ]; then
+    ~/cmake-3.16.5-Linux-x86_64/bin/ctest -VV -timeout 120
+else
+    ctest -VV -timeout 120
+fi
+
+
